aaabcf78e8af5ea5912b032ecc5f304ccda6bd65
regina <joyufeng@gmail.com>
2022-04-25 22:17:33 +0100

    amended 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import * as React from "react"
 import Head from "../components/head"
 import Layout from "../components/layout"
+import Error from "../assets/images/404.jpeg"
 
 export default function PageNotFound() {
   return (
     <Layout>
       <Head title="404" />
-      <h1>Sorry, something went wrong</h1>
+      <div className="container my-5">
+        <h2 className="section-title">
+          Oopsy daisy, something went wrong here.
+        </h2>
+        <div className="bottom-line"></div>
+        <div className="w-50 m-auto">
+          <img src={Error} alt="Error" />
+        </div>
+      </div>
     </Layout>
   )
 }
